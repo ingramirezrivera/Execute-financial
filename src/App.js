4ab4e37b6580488d61cd9a7856e7b8318e559180
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-10-05 16:18:19 -0500

    Sections About Testimonials and Demo Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Testimonials from "./components/Testimonials";
+import Demo from "./components/Demo";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
+      <Testimonials />
+      <Demo />
     </div>
   );
 }
