90a6095743f72bc0f7b18dad464b99e93f5c4b71
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-10-04 23:57:48 -0500

    Working on Header and Hero sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
